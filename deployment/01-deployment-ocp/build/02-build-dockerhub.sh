2512e7446d1dc32f62649ed556bfa48c95972871
r-ranjeet-kakade <98015201+r-ranjeet-kakade@users.noreply.github.com>
2022-12-26 20:34:56 +0530

    Update 02-build-dockerhub.sh

--- deployment/01-deployment-ocp/build/02-build-dockerhub.sh
+++ deployment/01-deployment-ocp/build/02-build-dockerhub.sh
@@ -11,15 +11,20 @@ echo "RootPath value is ............> $rootPATH"
 
 # docker login -u gandhicloudlab
 
+# # Docker Image related variables
+# # Example : docker.io/gandhicloudlab/welathcare-web-ocp-oss:0.0.1
+# export IMAGE_SUFFIX=ocp-oss:0.0.1
+# export REGISTRY_USER=$(oc whoami)
+# export NAMESPACE=wealthcare-ns
+# export OPENSHIFT_REGISTRY_NAME=$(oc get route -n openshift-image-registry image-registry | awk '{print $2}' | awk 'NR==2')
+
+# # Logging to openshift image registry 
+# docker login -u $(oc whoami) -p $(oc whoami -t) ${OPENSHIFT_REGISTRY_NAME}
+
 # Docker Image related variables
 # Example : docker.io/gandhicloudlab/welathcare-web-ocp-oss:0.0.1
 export IMAGE_SUFFIX=ocp-oss:0.0.1
-export REGISTRY_USER=$(oc whoami)
-export NAMESPACE=wealthcare-ns
-export OPENSHIFT_REGISTRY_NAME=$(oc get route -n openshift-image-registry image-registry | awk '{print $2}' | awk 'NR==2')
-
-# Logging to openshift image registry 
-docker login -u $(oc whoami) -p $(oc whoami -t) ${OPENSHIFT_REGISTRY_NAME}
+export REGISTRY_USER=gandhicloudlab
 
 cd $rootPATH/wealthweb/config
 sh 02-build-dockerhub.sh
