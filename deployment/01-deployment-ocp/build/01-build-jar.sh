17f929150a2f9c61b83d1766ce72bfdad68e33e2
r-ranjeet-kakade <98015201+r-ranjeet-kakade@users.noreply.github.com>
2022-12-27 23:51:49 +0530

    Update 01-build-jar.sh

--- deployment/01-deployment-ocp/build/01-build-jar.sh
+++ deployment/01-deployment-ocp/build/01-build-jar.sh
@@ -1,24 +1,40 @@
 #!/usr/bin/env bash
 
-echo "build all Started ...."
+echo "========================================================================="
+echo "Maven Build Started"
 
+echo "--------------------------------------------------------------------------"
 cdir=$(dirname "$0")
 folderPath=$(cd ${cdir}; pwd -P)
 cd "$folderPath"
 cd ../../../
 rootPATH="$(pwd)"
-echo "RootPath value is ............> $rootPATH"
+echo "Root Path value is -----> $rootPATH"
+echo "--------------------------------------------------------------------------"
 
+echo "--------------------------------------------------------------------------"
 cd $rootPATH/wealthweb
 mvn clean package
+echo "Wealth Web Maven Build COMPLETED"
+echo "--------------------------------------------------------------------------"
 
+echo "--------------------------------------------------------------------------"
 cd $rootPATH/wealthusers
 mvn clean package
+echo "Wealth Users Maven Build COMPLETED"
+echo "--------------------------------------------------------------------------"
 
+echo "--------------------------------------------------------------------------"
 cd $rootPATH/wealthnotification
 mvn clean package
+echo "Wealth Notification Maven Build COMPLETED"
+echo "--------------------------------------------------------------------------"
 
+echo "--------------------------------------------------------------------------"
 cd $rootPATH/wealthfinancialplan
 mvn clean package
+echo "Wealth Financial Plan Maven Build COMPLETED"
+echo "--------------------------------------------------------------------------"
 
-echo "build all completed ...."
+echo "ALL MAVEN BUILD COMPLETED"
+echo "========================================================================="
